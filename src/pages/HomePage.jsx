cbbddd46fb197089949f7c7edcbed92b9cfe5052
borderlesstravels <borderlesstravels.africa@gmail.com>
2024-05-28 13:09:00 +0100

    ads app completed 2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,9 +9,7 @@ const HomePage = () => {
       <div className="h-[100%] bg-[#030811] py-[60px] md:py-[100px] px-7 w-[100vw] text-center">
         <div>
           <h1 className="mb-3">Welcome here</h1>
-          <Link to="/add-1" className=" text-[#7e8000] underline">
-            View first published ad
-          </Link>
+          Ask for the ads link from our Telegram group.
         </div>
       </div>
     </>
